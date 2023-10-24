7a67f3bd16b2f4b97ea0efe5f9ccdf0ef1ec2b2b
Flagg217 <flagg217@gmail.com>
2023-10-24 04:01:05 -0700

    created files and started questions

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+let questions = [
+    {
+        number: 1,
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        answer: "script",
+        options: [
+            "scripted",
+            "script",
+            "hotcoffee",
+            "javascript"
+        ]   },
+    {
+        
