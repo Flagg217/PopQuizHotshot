8317db0639cab13f6e39dbf9dc12dfe472d81e7a
Flagg217 <flagg217@gmail.com>
2023-10-24 16:53:46 -0700

    set up some of the variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,4 +5,16 @@ var questionEl = document.getElementById("question");
 var timerEl = document.getElementById("timer");
 var choiceList = document.getElementById("choice-list");
 var theEnd = document.getElementById("the-end");
+var initials = document.getElementById("initials");
+var submitButton = document.getElementById("submit-button");
+var highScores = document.getElementById("high-scores");
+var scoreList = document.getElementById("score-list");
+var goBack = document.getElementById("go-back");
+var clearScores = document.getElementById("clear-scores");
+var restart = document.getElementById("restart");
+var currentQuestion = document.getElementById("current-question");
+var finalScore = document.getElementById("final-score");
+var score = 0;
+var scoreArray = JSON.parse(localStorage.getItem("scoreArray")) || [];
+
 
