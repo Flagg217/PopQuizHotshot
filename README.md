4db8e87c883603d9ffba8394ec2503ca44ffc826
Flagg217 <flagg217@gmail.com>
2023-10-24 19:45:58 -0700

    wrote readme

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # PopQuizHotshot
-coding quiz
+
+
+## Description: 
+
+This is a code quiz game. The goal is to answer as many questions correctly as possible before the timer runs out. If you answer a question incorrectly, time will be subtracted from the clock. The game is over when all questions have been answered or the timer reaches 0. At the end of the game, you can save your initials and score.
+
+
+## How to play:
+
+1. Click the "Start" button to start the game.
+2. Answer the questions that appear on the screen.
+3. If you answer a question correctly, you will be presented with the next question.
+4. If you answer a question incorrectly, time will be subtracted from the clock.
+5. The game is over when all questions have been answered or the timer reaches 0.
+6. At the end of the game, you can save your initials and score.
+
+
+## Features:
+
+- A timer to keep track of your progress
+- The ability to save your initials and score
+
+
+## Prerequisites:
+
+- A web browser
+
+
+## Installation:
+
+No installation required. Simply open the game in a web browser.
+
+
+## Usage:
+
+1. Click the "Start" button to start the game.
+2. Answer the questions that appear on the screen.
+3. If you answer a question correctly, you will be presented with the next question.
+4. If you answer a question incorrectly, time will be subtracted from the clock.
+5. The game is over when all questions have been answered or the timer reaches 0.
+6. At the end of the game, you can save your initials and score.
+
+
+## License:
+
+This game is licensed under the MIT License.
+
+
+## Screenshot:
+
+![Screenshot](./assets/QuizScreenshot.png)
+
+
+## Links:
+
+- [GitHub Repository]()
+- [Deployed Application]()
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -25,99 +25,103 @@ let widthValue = 0;
 
 var initialsValue;
 
-document.getElementById("submit").addEventListener("click", function() {
-    event.preventDefault();
-    theEnd.style.display = "none";
-    highScores.style.display = "block";
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault();
+  theEnd.style.display = "none";
+  highScores.style.display = "block";
 
-    initialsValue = initials.value;
-    var scoreString = "";
-   
-    for (var i = 0; i < scoreArray.length; i++) {
-        scoreString += "Initials: " + initialsValue + " - Score: " + score[i] + "<br>";}
+  initialsValue = initials.value;
+  var scoreString = "";
 
-    scoreList.innerHTML = scoreString;
-    });
+  for (var i = 0; i < scoreArray.length; i++) {
+    scoreString +=
+      "Initials: " + initialsValue + " - Score: " + score[i] + "<br>";
+  }
+
+  scoreList.innerHTML = scoreString;
+});
 
 function nextQuestion() {
-    questionCount++;
-    if (questionCount >= questions.length) {
-        continueOn();
-        clearInterval(timeInterval);
-    } else {
-        displayQuestion();
-}
+  questionCount++;
+  if (questionCount >= questions.length) {
+    continueOn();
+    clearInterval(timeInterval);
+  } else {
+    displayQuestion();
+  }
 }
 
 function displayQuestion() {
-    currentQuestion.textContent = questions[questionCount].question;
+  currentQuestion.textContent = questions[questionCount].question;
 
-    while (choiceList.firstChild) {
-        choiceList.removeChild(choiceList.firstChild);
-    }
+  while (choiceList.firstChild) {
+    choiceList.removeChild(choiceList.firstChild);
+  }
 
-    for (let i = 0; i < questions[questionCount].options.length; i ++) {
-        var newBtn = document.createElement("button");
-        newBtn.textContent = questions[questionCount].options[i];
+  for (let i = 0; i < questions[questionCount].options.length; i++) {
+    var newBtn = document.createElement("button");
+    newBtn.textContent = questions[questionCount].options[i];
 
-        newBtn.addEventListener("click", function (event) {
-            event.preventDefault();
-            var userChoice = questions[questionCount].userChoice;
+    newBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      var userChoice = questions[questionCount].userChoice;
 
-            if (event.target.textContent !== userChoice) {
-                timeValue -= 10;
-                if (timeValue <= 0) {
-                    clearInterval(timeInterval);
-                    continueOn();
-                }
-            } else {
-                score += 10;
-            }
-            nextQuestion();
+      if (event.target.textContent !== userChoice) {
+        timeValue -= 10;
+        if (timeValue <= 0) {
+          clearInterval(timeInterval);
+          continueOn();
+        }
+      } else {
+        score += 10;
+      }
+      nextQuestion();
     });
     choiceList.appendChild(newBtn);
-}
+  }
 }
 
 function continueOn() {
-    contentArea.style.display = "none";
-    theEnd.style.display = "block";
-    finalScore.textContent = "Your final score is " + score;
+  contentArea.style.display = "none";
+  theEnd.style.display = "block";
+  finalScore.textContent = "Your final score is " + score;
 
-    finalScore = [];
+  finalScore = [];
 
-    scoreArray.push(score);
-    localStorage.setItem("score", JSON.stringify(scoreArray));
+  scoreArray.push(score);
+  localStorage.setItem("score", JSON.stringify(scoreArray));
 }
 
-document.getElementById("start-button").addEventListener("click", function (event) {
+document
+  .getElementById("start-button")
+  .addEventListener("click", function (event) {
     container.style.display = "none";
     contentArea.style.display = "block";
 
     timeInterval = setInterval(function () {
-        timeValue -= 1;
+      timeValue -= 1;
 
-        if (timeValue < 0) {
-            clearInterval(timeInterval);
-            continueOn();
-        }
+      if (timeValue < 0) {
+        clearInterval(timeInterval);
+        continueOn();
+      }
 
-        timerEl.textContent = timeValue;
+      timerEl.textContent = timeValue;
     }, 1000);
 
     displayQuestion();
-});
+  });
 
 restart.addEventListener("click", function (event) {
-    location.reload();
+  location.reload();
 });
 
 goBack.addEventListener("click", function (event) {
-    highScores.style.display = "none";
-    container.style.display = "block";
+  highScores.style.display = "none";
+  container.style.display = "block";
 });
 
 clearScores.addEventListener("click", function (event) {
-    localStorage.clear("score");
-    scoreList.innerHTML = "";
+  localStorage.clear("score");
+  scoreList.innerHTML = "";
 });
